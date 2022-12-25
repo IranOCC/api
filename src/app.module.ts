8c747698ab93d57767cf51168f356b8aef04a32a
rasoul707 <r.ahmadifar.1377@gmail.com>
2022-12-26 00:12:09 +0330

    add minio upload file

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { MinioClientModule } from './minio-client/minio-client.module';
+import { ImageUploadModule } from './image-upload/image-upload.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
+    MinioClientModule,
     MongooseModule.forRoot(process.env.MONGODB_URL),
+    ImageUploadModule,
   ],
   controllers: [AppController],
   providers: [AppService],
