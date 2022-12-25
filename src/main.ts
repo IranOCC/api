b375992f49d6b6270d918964fb91b24614e1ec43
rasoul707 <r.ahmadifar.1377@gmail.com>
2022-12-25 11:40:26 +0330

    add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('IranOCC-API')
+    .setDescription('The API of iranocc.com')
+    .setVersion('1.0')
+    .addTag('iranocc')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
