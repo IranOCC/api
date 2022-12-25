b01625df002f31c08c2f79076de12d46f4f65bc9
rasoul707 <r.ahmadifar.1377@gmail.com>
2022-12-25 12:45:34 +0330

    add env and mongo

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+const VERSION = 'v1';
+const VERSION_NUMBER = '1.0';
+const DOCUMENT_TITLE = 'iranocc-api';
+const DOCUMENT_DESCRIPTION = 'The API of iranocc.com';
+const DOCUMENT_PATH = 'api/v1/doc';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1');
-
+  app.setGlobalPrefix(`api/${VERSION}`);
   const options = new DocumentBuilder()
-    .setTitle('IranOCC-API')
-    .setDescription('The API of iranocc.com')
-    .setVersion('1.0')
+    .setTitle(DOCUMENT_TITLE)
+    .setDescription(DOCUMENT_DESCRIPTION)
+    .setVersion(VERSION_NUMBER)
     .addTag('iranocc')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api/v1/doc', app, document);
+  SwaggerModule.setup(DOCUMENT_PATH, app, document);
 
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
