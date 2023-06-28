96acd08806c7c169398a13a23df8a53edf26f3ea
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-06-28 10:31:13 +0330

    fixed phone number

--- src/phone/dto/phoneNumber.dto.ts
+++ src/phone/dto/phoneNumber.dto.ts
@@ -4,13 +4,21 @@ import { IsPhoneNumber } from 'class-validator';
 import { i18nValidationMessage as i18nVM } from 'nestjs-i18n';
 
 
-
+// 9212728307
+// 09212728307
+// 989212728307
+// +989212728307
+// 00989212728307
 
 export class PhoneNumberDto {
     @ApiProperty()
     @Transform(({ value }) => {
-        if (value.length === 10) value = '0' + value;
-        return value.replace(/^0/, "+98");
+        if (value.substring(0, 1) === "0") value = value.substring(1);
+        else if (value.substring(0, 2) === "98") value = value.substring(2);
+        else if (value.substring(0, 3) === "+98") value = value.substring(3);
+        else if (value.substring(0, 3) === "098") value = value.substring(3);
+        else if (value.substring(0, 4) === "0098") value = value.substring(4);
+        return "+98" + value;
     })
     @IsPhoneNumber("IR", {
         message: i18nVM(`validation.IsPhoneNumber`),
@@ -23,8 +31,12 @@ export class PhoneNumberDto {
 export class PhoneValueDto {
     @ApiProperty()
     @Transform(({ value }) => {
-        if (value.length === 10) value = '0' + value;
-        return value.replace(/^0/, "+98");
+        if (value.substring(0, 1) === "0") value = value.substring(1);
+        else if (value.substring(0, 2) === "98") value = value.substring(2);
+        else if (value.substring(0, 3) === "+98") value = value.substring(3);
+        else if (value.substring(0, 3) === "098") value = value.substring(3);
+        else if (value.substring(0, 4) === "0098") value = value.substring(4);
+        return "+98" + value;
     })
     @IsPhoneNumber("IR", {
         message: i18nVM(`validation.IsPhoneNumber`),
