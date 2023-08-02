288b6e312467d59523a9d6e1f9dab25d5763aeeb
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-08-02 13:07:25 +0330

    fix some feature to list estate

--- src/estate/estate/public/estate.public.service.ts
+++ src/estate/estate/public/estate.public.service.ts
@@ -56,7 +56,7 @@ export class EstatePublicService {
       ["storages", "image", "path alt title", false],
       ["storages", "gallery", "path alt title", true],
     ]
-    const project = "title slug excerpt area canBarter buildingArea roomCount mastersCount buildingArea floorsCount unitsCount floor withOldBuilding publishedAt createdAt code province city district"
+    const project = "title slug excerpt area canBarter buildingArea roomsCount mastersCount constructionYear buildingArea floorsCount unitsCount floor withOldBuilding publishedAt createdAt code province city district"
     const virtualFields = {}
     const searchFields = "title slug excerpt content code province city district quarter alley address"
     if (!filter) filter = {}
