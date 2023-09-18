7970d4c3d57f741880865a92d54138f7f14adeef
rasoul707 <r.ahmadifar.1377@gmail.com>
2023-09-18 17:30:09 +0330

    fixed posts

--- src/dashboard/dashboard.module.ts
+++ src/dashboard/dashboard.module.ts
@@ -3,10 +3,12 @@ import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Estate, EstateSchema } from 'src/estate/estate/schemas/estate.schema';
+import { BlogPost, BlogPostSchema } from 'src/blog/post/schemas/blogPost.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Estate.name, schema: EstateSchema }]),
+    MongooseModule.forFeature([{ name: BlogPost.name, schema: BlogPostSchema }]),
   ],
   controllers: [DashboardController],
   providers: [DashboardService],
